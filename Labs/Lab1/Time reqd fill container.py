47d10fc27fc664f5100de56a25ea5fc0df3d4e9d
yuvrajgupta1 <guptayuvraj16@gmail.com>
2021-03-24 07:01:43 +0530

    Delete Files

--- Labs/Lab1/Time reqd fill container.py
+++
@@ -1,8 +0,0 @@
-s = 0.4
-v = input("Enter the volume of container in cubic metres\n")
-v = float(v)
-v *= 1000   #convert cubic metres to litres
-
-time = (v / s) * 60
-
-print("The time required to fill container is ", time, " seconds")
