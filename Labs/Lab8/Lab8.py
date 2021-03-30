930e9f3e5fb36a95a77f641ac7ebe8a7324421d6
yuvrajgupta1 <guptayuvraj16@gmail.com>
2021-03-31 03:37:43 +0530

    Added additional comments

--- Labs/Lab8/Lab8.py
+++ Labs/Lab8/Lab8.py
@@ -1,33 +1,52 @@
 """COMP 150 Lab 8 written by Yuvraj Gupta on March 23, 2021"""
 
 
+# Read file function
 def readFile(filename, lst):
+    # open file
     fhand = open(filename)
+
+    # reading file and putting numbers into a list
     for line in fhand:
         lst.append(int(line))
+
+    # file closed
     fhand.close()
+
     return lst
 
 
+# comparing lists
 def equal_num(lst1, lst2):
+    # checking if two elements in a list are equal
     for i in lst1:
         for j in lst2:
             if i == j:
                 lst3.append(i)
+
     return lst3
 
 
+# Write into a file function
 def writeFile(filename):
+    # open/create file in write mode
     fhand = open(filename, "w")
+
+    # writing dadta into file
     for i in lst3:
         fhand.write(str(i)+"\n")
     print(lst3)
+
+    # file close
     fhand.close()
 
 
+# list initializations
 lst1 = []
 lst2 = []
 lst3 = []
+
+# function calls
 readFile("primenumbers.txt", lst1)
 readFile("happynumbers.txt", lst2)
 equal_num(lst1, lst2)
